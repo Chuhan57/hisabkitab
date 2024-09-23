11f2e2e93bfb5117e48b23c3de0393ddcaf4c61d
Chuhan57 <chuhan575727@gmail.com>
2024-09-23 01:38:50 -0700

    Update settings.py

--- hisabkitab/settings.py
+++ hisabkitab/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6w2bomm_w0+kv==_hrjwvx2_*ep#e9p1sp%dw&3i)ytr1svt)k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
